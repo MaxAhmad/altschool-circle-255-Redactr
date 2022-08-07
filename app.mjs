d1c60038a306f9d2c8bd7fe4c9dc422aa848159c
Max Ahmad <ahmadmax046@gmail.com>
2022-08-07 10:26:40 +0000

    refactor JS files

--- app.mjs
+++ app.mjs
@@ -23,29 +23,8 @@ function startApp() {
   let wordCount = document.querySelector('.totalWords')
   let wordMatch = document.querySelector('.totalMatch')
   let timeRun = document.querySelector('.totalTime')
-  
-
-
-  // let ST = inputValue.value
-  // let MT = inputText.value
-  // let stArr = ST.split(' ')
-  // console.log(MT)
-  // console.log(ST)
 
   const btn = document.querySelector('.btn')
-
-  function ValidateSelection() {
-    var checkboxes = document.getElementsByClassName("check");
-    var numberOfCheckedItems = 0;
-    for (var i = 0; i < checkboxes.length; i++) {
-      if (checkboxes[i].checked)
-        numberOfCheckedItems++;
-    }
-    if (numberOfCheckedItems > 1) {
-      alert("You can't select more than one symbol!");
-      return false
-    }
-  }
 
   btn.addEventListener('click', () => {
     event.preventDefault()
@@ -54,26 +33,18 @@ function startApp() {
 
     let ST = inputValue.value //ST = scramble text
     let MT = inputText.value //MT = main text
-    let stArr = ST.split(' ') //stArr = scramble text array
-    let mtArr = MT.split(' ')
-
+   
 // code to get number of words scrambled
-    console.log(mtArr.length)
-    console.log(MT)
-    console.log(stArr)
-    console.log(MT.length)
-    
+    let mtArr = MT.split(' ')
     wordCount.innerHTML = mtArr.length 
 
 // code to get length of characters scanned
+     let stArr = ST.split(' ') //stArr = scramble text array
     let strLength = stArr.join('')
-    console.log(strLength)
-    console.log(strLength.length)
-
     wordMatch.innerHTML = strLength.length
 
     
-
+    // code to get value of selected radio button
     if (check1.checked == true) {
       symbol = document.getElementById("check1").value.trim()
       console.log(symbol)
@@ -88,26 +59,26 @@ function startApp() {
       console.log(typeof symbol)
     }
     else {
-      console.log("You have not selected anything")
+      alert("You have not selected anything")
     }
+
+    // code to scramble words selected
     for (let i = 0; i < stArr.length; i++) {
       while (MT.includes(stArr[i])) {
         MT = MT.replace(stArr[i],
           symbol.repeat(stArr[i].length))
       }
     }
-    console.log(MT)
+
+    // codde to display scrambled words with text
     paragragh.innerHTML = MT
 
     var t1 = performance.now();
-    console.log("Your Words was scrambled in  " + (t1 - t0) + " milliseconds.")
 
     var time = t1-t0;
     time = +time.toFixed(2);
     
     timeRun.innerHTML = time + ' ms'
-    
-
   })
 }
 
